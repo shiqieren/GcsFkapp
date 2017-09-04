287fd2efb1221f149492c17e2c0f15d8d67a1392
liyiwei <365355497@qq.com>
2017-09-04 22:21:11 +0800

    api 交互

--- app/src/main/java/com/gcs/fengkong/ui/frags/subfrags/BankAuthFragment.java
+++ app/src/main/java/com/gcs/fengkong/ui/frags/subfrags/BankAuthFragment.java
@@ -1,9 +1,19 @@
 package com.gcs.fengkong.ui.frags.subfrags;
 
 import android.annotation.SuppressLint;
+import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.gcs.fengkong.R;
+import com.gcs.fengkong.ui.account.RichTextParser;
 import com.gcs.fengkong.ui.atys.SimpleBackActivity;
 import com.gcs.fengkong.ui.frags.BaseFragment;
 
@@ -11,7 +21,25 @@ import com.gcs.fengkong.ui.frags.BaseFragment;
  * Created by Administrator on 0029 8-29.
  */
 @SuppressLint("NewApi")
-public class BankAuthFragment extends BaseFragment{
+public class BankAuthFragment extends BaseFragment implements View.OnClickListener, View.OnFocusChangeListener{
+
+    private LinearLayout mLlBankcardName;
+    private LinearLayout mLlBankcardNumber;
+    private LinearLayout mLlBankcardPhone;
+    private EditText mEtBankcardName;
+    private EditText mEtBankcardNumber;
+    private EditText mEtBankcardPhone;
+    private ImageView mIvBankcardNameDel;
+    private ImageView mIvBankcardNumberDel;
+    private ImageView mIvBankcardPhoneDel;
+    private TextView mTvBankcardSmsCall;
+    private LinearLayout mLlBankcardSmsCode;
+    private EditText mEtBankcardSmsCode;
+    private Button mBtBankcardSubmit;
+
+    private boolean mMachPhoneNum;
+
+    private CountDownTimer mTimer;
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_bankcard_auth;
@@ -20,6 +48,253 @@ public class BankAuthFragment extends BaseFragment{
     protected void initView(View view) {
         super.initView(view);
         ((SimpleBackActivity)getActivity()).setToolBarTitle(R.string.bank_string);
+
+        mLlBankcardName  = view.findViewById(R.id.ll_bankcard_name);
+        mLlBankcardNumber  = view.findViewById(R.id.ll_bankcard_number);
+        mLlBankcardPhone  = view.findViewById(R.id.ll_bankcard_phone);
+
+        mEtBankcardName =  view.findViewById(R.id.et_bankcard_name);
+        mEtBankcardNumber =  view.findViewById(R.id.et_bankcard_number);
+        mEtBankcardPhone =  view.findViewById(R.id.et_bankcard_phone);
+
+        mIvBankcardNameDel =  view.findViewById(R.id.iv_bankcard_name_del);
+        mIvBankcardNumberDel =  view.findViewById(R.id.iv_bankcard_number_del);
+        mIvBankcardPhoneDel =  view.findViewById(R.id.iv_bankcard_phone_del);
+
+        mBtBankcardSubmit =  view.findViewById(R.id.bt_bankcard_submit);
+
+        mTvBankcardSmsCall= view.findViewById(R.id.tv_bankcard_sms_call);
+        mLlBankcardSmsCode= view.findViewById(R.id.ll_bankcard_sms_code);
+        mEtBankcardSmsCode=  view.findViewById(R.id.et_bankcard_sms_code);
+
+        setListener();
+        mEtBankcardName.setOnFocusChangeListener(this);
+        mEtBankcardName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void afterTextChanged(Editable s) {
+                String username = s.toString().trim();
+                if (username.length() > 0) {
+                    //   mLlIdentityName.setBackgroundResource(R.drawable.bg_login_input_ok);
+                    mIvBankcardNameDel.setVisibility(View.VISIBLE);
+                } else {
+                    //   mLlIdentityName.setBackgroundResource(R.drawable.bg_login_input_ok);
+                    mIvBankcardNameDel.setVisibility(View.INVISIBLE);
+                }
+                String phone = mEtBankcardPhone.getText().toString().trim();
+                String identitynumber = mEtBankcardNumber.getText().toString().trim();
+                String pwd = mEtBankcardNumber.getText().toString().trim();
+                if (!TextUtils.isEmpty(identitynumber)&&!TextUtils.isEmpty(pwd)&&!TextUtils.isEmpty(phone)) {
+                    mBtBankcardSubmit.setEnabled(true);
+                   /* mBtLoginSubmit.setBackgroundResource(R.drawable.bg_login_submit);
+                    mBtLoginSubmit.setTextColor(getResources().getColor(R.color.white));*/
+                } else {
+                    mBtBankcardSubmit.setEnabled(false);
+                  /*  mBtLoginSubmit.setBackgroundResource(R.drawable.bg_login_submit_lock);
+                    mBtLoginSubmit.setTextColor(getResources().getColor(R.color.account_lock_font_color));*/
+                }
+
+            }
+        });
+
+        mEtBankcardNumber.setOnFocusChangeListener(this);
+        mEtBankcardNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+            }
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void afterTextChanged(Editable s) {
+                int length = s.length();
+                if (length > 0) {
+                    // mLlIdentityNumber.setBackgroundResource(R.drawable.bg_login_input_ok);
+                    mIvBankcardNumberDel.setVisibility(View.VISIBLE);
+                } else {
+                    mIvBankcardNumberDel.setVisibility(View.INVISIBLE);
+                }
+
+                String pwd = mEtBankcardNumber.getText().toString().trim();
+                String identitynumber = mEtBankcardNumber.getText().toString().trim();
+                String phone = mEtBankcardPhone.getText().toString().trim();
+                if (!TextUtils.isEmpty(pwd)&&!TextUtils.isEmpty(identitynumber)&&!TextUtils.isEmpty(phone)) {
+                    mBtBankcardSubmit.setEnabled(true);
+                   /* mBtLoginSubmit.setBackgroundResource(R.drawable.bg_login_submit);
+                    mBtLoginSubmit.setTextColor(getResources().getColor(R.color.white));*/
+                }else {
+                    mBtBankcardSubmit.setEnabled(false);
+                   /* mBtLoginSubmit.setBackgroundResource(R.drawable.bg_login_submit_lock);
+                    mBtLoginSubmit.setTextColor(getResources().getColor(R.color.account_lock_font_color));*/
+                }
+            }
+        });
+
+        mEtBankcardPhone.setOnFocusChangeListener(this);
+        mEtBankcardPhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+            }
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void afterTextChanged(Editable s) {
+                int length = s.length();
+                String input = s.toString();
+                mMachPhoneNum = RichTextParser.machPhoneNum(input);
+
+                if (mMachPhoneNum) {
+                    String smsCode = mEtBankcardSmsCode.getText().toString().trim();
+                    String pwd = mEtBankcardNumber.getText().toString().trim();
+                    if (!TextUtils.isEmpty(smsCode)&&!TextUtils.isEmpty(pwd)) {
+                        mBtBankcardSubmit.setEnabled(true);
+                        /*mBtBankcardSubmit.setBackgroundResource(R.drawable.bg_login_submit);
+                        mBtBankcardSubmit.setTextColor(getResources().getColor(R.color.white));*/
+                    } else {
+                        mBtBankcardSubmit.setEnabled(false);
+                      /*  mBtBankcardSubmit.setBackgroundResource(R.drawable.bg_login_submit_lock);
+                        mBtBankcardSubmit.setTextColor(getResources().getColor(R.color.account_lock_font_color));*/
+                    }
+                } else {
+                    mBtBankcardSubmit.setEnabled(false);
+                    /*mBtBankcardSubmit.setBackgroundResource(R.drawable.bg_login_submit_lock);
+                    mBtBankcardSubmit.setTextColor(getResources().getColor(R.color.account_lock_font_color));*/
+                }
+
+                if (length > 0 && length < 11) {
+                    mLlBankcardPhone.setBackgroundResource(R.drawable.bg_login_input_error);
+                    mTvBankcardSmsCall.setAlpha(0.4f);
+                } else if (length == 11) {
+                    if (mMachPhoneNum) {
+                        mLlBankcardPhone.setBackgroundResource(R.drawable.bg_login_input_ok);
+                        if (mTvBankcardSmsCall.getTag() == null) {
+                            mTvBankcardSmsCall.setAlpha(1.0f);
+                        } else {
+                            mTvBankcardSmsCall.setAlpha(0.4f);
+                        }
+                    } else {
+                        mLlBankcardPhone.setBackgroundResource(R.drawable.bg_login_input_error);
+                        //showToastForKeyBord(R.string.hint_username_ok);
+                        mTvBankcardSmsCall.setAlpha(0.4f);
+                    }
+                } else if (length > 11) {
+                    mTvBankcardSmsCall.setAlpha(0.4f);
+                    mLlBankcardPhone.setBackgroundResource(R.drawable.bg_login_input_error);
+                } else if (length <= 0) {
+                    mTvBankcardSmsCall.setAlpha(0.4f);
+                    mLlBankcardPhone.setBackgroundResource(R.drawable.bg_login_input_ok);
+                }
+            }
+        });
     }
 
+    private void setListener() {
+        mLlBankcardName.setOnClickListener(this);
+        mEtBankcardName.setOnClickListener(this);
+        mIvBankcardNameDel.setOnClickListener(this);
+        mLlBankcardPhone.setOnClickListener(this);
+        mEtBankcardPhone.setOnClickListener(this);
+        mIvBankcardPhoneDel.setOnClickListener(this);
+        mTvBankcardSmsCall.setOnClickListener(this);
+        mLlBankcardNumber.setOnClickListener(this);
+        mEtBankcardNumber.setOnClickListener(this);
+        mIvBankcardNumberDel.setOnClickListener(this);
+        mBtBankcardSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.et_bankcard_name:
+                mEtBankcardNumber.clearFocus();
+                mEtBankcardName.setFocusableInTouchMode(true);
+                mEtBankcardName.requestFocus();
+                break;
+            case R.id.et_bankcard_number:
+                mEtBankcardName.clearFocus();
+                mEtBankcardNumber.setFocusableInTouchMode(true);
+                mEtBankcardNumber.requestFocus();
+                break;
+            case R.id.et_bankcard_phone:
+                mEtBankcardName.clearFocus();
+                mEtBankcardNumber.setFocusableInTouchMode(true);
+                mEtBankcardNumber.requestFocus();
+                break;
+            case R.id.tv_bankcard_sms_call:
+                requestSmsCode();
+                break;
+            case R.id.bt_bankcard_submit:
+                AuthBankcardRequest();
+                break;
+
+            case R.id.iv_bankcard_name_del:
+                mEtBankcardName.setText(null);
+                break;
+            case R.id.iv_bankcard_number_del:
+                mEtBankcardNumber.setText(null);
+                break;
+            case R.id.iv_bankcard_phone_del:
+                mEtBankcardPhone.setText(null);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void requestSmsCode() {
+    }
+
+    private void AuthBankcardRequest() {
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        int id = v.getId();
+        switch (id){
+            case R.id.et_bankcard_name:
+                if (hasFocus) {
+                    mLlBankcardName.setActivated(true);
+                    mLlBankcardNumber.setActivated(false);
+                    mLlBankcardPhone.setActivated(false);
+                }
+                break;
+            case  R.id.et_bankcard_number:
+                if (hasFocus) {
+                    mLlBankcardNumber.setActivated(true);
+                    mLlBankcardName.setActivated(false);
+                    mLlBankcardPhone.setActivated(false);
+                }
+            case  R.id.et_bankcard_phone:
+                if (hasFocus) {
+                    mLlBankcardNumber.setActivated(false);
+                    mLlBankcardName.setActivated(false);
+                    mLlBankcardPhone.setActivated(true);
+                }
+        }
+
+    }
 }
--- app/src/main/java/com/gcs/fengkong/ui/frags/subfrags/IdentityAuthFragment.java
+++ app/src/main/java/com/gcs/fengkong/ui/frags/subfrags/IdentityAuthFragment.java
@@ -3,10 +3,19 @@ package com.gcs.fengkong.ui.frags.subfrags;
 import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.gcs.fengkong.R;
+import com.gcs.fengkong.ui.account.atys.LoginActivity;
+import com.gcs.fengkong.ui.account.atys.RegisterActivity;
+import com.gcs.fengkong.ui.account.atys.ResetPwdActivity;
 import com.gcs.fengkong.ui.atys.SimpleBackActivity;
 import com.gcs.fengkong.ui.frags.BaseFragment;
 
@@ -14,26 +23,173 @@ import com.gcs.fengkong.ui.frags.BaseFragment;
  * Created by Administrator on 0029 8-29.
  */
 @SuppressLint("NewApi")
-public class IdentityAuthFragment extends BaseFragment{
+public class IdentityAuthFragment extends BaseFragment implements View.OnClickListener, View.OnFocusChangeListener{
 
+    private LinearLayout mLlIdentityName;
+    private LinearLayout mLlIdentityNumber;
     private ImageView mIvIdentityNameDel;
     private ImageView mIvIdentityNumberDel;
+    private EditText mEtIdentityName;
+    private EditText mEtIdentityNumber;
+    private Button mBtIdentitySubmit;
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_identity_auth;
     }
 
+
     @Override
     protected void initView(View view) {
         super.initView(view);
         ((SimpleBackActivity)getActivity()).setToolBarTitle(R.string.identity_string);
-
+        mLlIdentityName = view.findViewById(R.id.ll_identity_name);
+        mLlIdentityNumber = view.findViewById(R.id.ll_identity_number);
         mIvIdentityNameDel = (ImageView) view.findViewById(R.id.iv_identity_name_del);
         mIvIdentityNumberDel = (ImageView) view.findViewById(R.id.iv_identity_number_del);
+       mEtIdentityName = view.findViewById(R.id.et_identity_name);
+       mEtIdentityNumber = view.findViewById(R.id.et_identity_number);
+        mBtIdentitySubmit = view.findViewById(R.id.bt_identity_submit);
+        setListener();
+        mEtIdentityName.setOnFocusChangeListener(this);
+        mEtIdentityName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void afterTextChanged(Editable s) {
+                String username = s.toString().trim();
+                if (username.length() > 0) {
+                 //   mLlIdentityName.setBackgroundResource(R.drawable.bg_login_input_ok);
+                    mIvIdentityNameDel.setVisibility(View.VISIBLE);
+                } else {
+                 //   mLlIdentityName.setBackgroundResource(R.drawable.bg_login_input_ok);
+                    mIvIdentityNameDel.setVisibility(View.INVISIBLE);
+                }
+
+                String identitynumber = mEtIdentityNumber.getText().toString().trim();
+                String pwd = mEtIdentityNumber.getText().toString().trim();
+                if (!TextUtils.isEmpty(identitynumber)&&!TextUtils.isEmpty(pwd)) {
+                    mBtIdentitySubmit.setEnabled(true);
+                   /* mBtLoginSubmit.setBackgroundResource(R.drawable.bg_login_submit);
+                    mBtLoginSubmit.setTextColor(getResources().getColor(R.color.white));*/
+                } else {
+                    mBtIdentitySubmit.setEnabled(false);
+                  /*  mBtLoginSubmit.setBackgroundResource(R.drawable.bg_login_submit_lock);
+                    mBtLoginSubmit.setTextColor(getResources().getColor(R.color.account_lock_font_color));*/
+                }
+
+            }
+        });
+
+        mEtIdentityNumber.setOnFocusChangeListener(this);
+        mEtIdentityNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+            }
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void afterTextChanged(Editable s) {
+                int length = s.length();
+                if (length > 0) {
+                   // mLlIdentityNumber.setBackgroundResource(R.drawable.bg_login_input_ok);
+                    mIvIdentityNumberDel.setVisibility(View.VISIBLE);
+                } else {
+                    mIvIdentityNumberDel.setVisibility(View.INVISIBLE);
+                }
+
+                String pwd = mEtIdentityNumber.getText().toString().trim();
+                String identitynumber = mEtIdentityNumber.getText().toString().trim();
+                if (!TextUtils.isEmpty(pwd)&&!TextUtils.isEmpty(identitynumber)) {
+                    mBtIdentitySubmit.setEnabled(true);
+                   /* mBtLoginSubmit.setBackgroundResource(R.drawable.bg_login_submit);
+                    mBtLoginSubmit.setTextColor(getResources().getColor(R.color.white));*/
+                }else {
+                    mBtIdentitySubmit.setEnabled(false);
+                   /* mBtLoginSubmit.setBackgroundResource(R.drawable.bg_login_submit_lock);
+                    mBtLoginSubmit.setTextColor(getResources().getColor(R.color.account_lock_font_color));*/
+                }
+            }
+        });
+    }
+
+    private void setListener() {
+        mLlIdentityName.setOnClickListener(this);
+        mEtIdentityName.setOnClickListener(this);
+        mIvIdentityNameDel.setOnClickListener(this);
+        mLlIdentityNumber.setOnClickListener(this);
+        mEtIdentityNumber.setOnClickListener(this);
+        mIvIdentityNumberDel.setOnClickListener(this);
+        mBtIdentitySubmit.setOnClickListener(this);
+    }
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.et_identity_name:
+                mEtIdentityNumber.clearFocus();
+                mEtIdentityName.setFocusableInTouchMode(true);
+                mEtIdentityName.requestFocus();
+                break;
+            case R.id.et_identity_number:
+                mEtIdentityName.clearFocus();
+                mEtIdentityNumber.setFocusableInTouchMode(true);
+                mEtIdentityNumber.requestFocus();
+                break;
+
+            case R.id.bt_identity_submit:
+                AuthIdentityRequest();
+                break;
+
+            case R.id.iv_identity_name_del:
+                mEtIdentityName.setText(null);
+                break;
+            case R.id.iv_identity_number_del:
+                mEtIdentityNumber.setText(null);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void AuthIdentityRequest() {
     }
 
     @Override
     protected void initData() {
         super.initData();
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        int id = v.getId();
+
+        if (id == R.id.et_identity_name) {
+            if (hasFocus) {
+                mLlIdentityName.setActivated(true);
+                mLlIdentityNumber.setActivated(false);
+            }
+        } else {
+            if (hasFocus) {
+                mLlIdentityNumber.setActivated(true);
+                mLlIdentityName.setActivated(false);
+            }
+        }
     }
 }
